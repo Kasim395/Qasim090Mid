f035ec3a84a9e66e63be07e4535cfca15d766191
Kasim395 <108619478+Kasim395@users.noreply.github.com>
2022-11-22 21:23:36 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import {  DefaultTheme , DarkTheme } from '@react-navigation/native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, FlatList } from 'react-native';
+
+
+import Screen2 from './script/Screen2.js';
+import Screen3 from './script/Screen3.js';
+import Home from './Home.js';
+
+
+const Stack = createNativeStackNavigator();
+
+
+
+export default function App(props) {
+
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator >
+
+        <Stack.Screen name="Home" component={Home}  />
+ 
+        <Stack.Screen name="Screen2" component={Screen2} />
+        <Stack.Screen name="Screen3" component={Screen3} />
+        
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
